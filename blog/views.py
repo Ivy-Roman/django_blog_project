c5ad4f1ffe34e0bf347aa306e6d936e8dfd6ba31
Ivie Osoiye <ivieosoiye@gmail.com>
2022-07-29 13:08:10 +0100

    deleted post dummy data

--- blog/views.py
+++ blog/views.py
@@ -2,23 +2,6 @@ from django.shortcuts import render
 from .models import Post
 # Create your views here.
 
-posts = [
-    {
-        'author': 'TRI',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27,2018'
-    },
-
-    {
-        'author': 'Noel King',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28,2018'
-    }
-]
-
-
 def home(request):
     context = {
         'posts': Post.objects.all()
