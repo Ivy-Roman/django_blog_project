48d5cc326f8f632bf3ecd443efa546175dfb326a
Ivie Osoiye <ivieosoiye@gmail.com>
2022-07-30 10:08:45 +0100

    imported new form class with additional fields into views

--- users/views.py
+++ users/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST) #instantiates a user creation form with that POST data
+        form = UserRegisterForm(request.POST) #instantiates a user creation form with that POST data
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
             return redirect('blog-home')
     else:
-        form = UserCreationForm() #instantiates an empty form
+        form = UserRegisterForm() #instantiates an empty form
     return render(request, 'users/register.html', {'form': form})
