c0dc9b9f890da3acf15c51a59198c9c065050327
Ivie Osoiye <ivieosoiye@gmail.com>
2022-08-04 12:42:02 +0100

    created a new class view for creating posts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 
@@ -9,14 +9,22 @@ def home(request):
     }
     return render(request, 'blog/home.html', context)
 
+
 class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
 
+
 class PostDetailView(DetailView):
     model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+    
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
