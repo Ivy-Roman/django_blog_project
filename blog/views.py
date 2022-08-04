064729788bbca5d05ae5c28a94fdb709bf657ec9
Ivie Osoiye <ivieosoiye@gmail.com>
2022-08-04 15:05:11 +0100

    created a new view class for updating posts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post
 
@@ -31,5 +31,14 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+ 
+ 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
