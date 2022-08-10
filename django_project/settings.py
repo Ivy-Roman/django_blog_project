4eceb5d734df9e50a14f3db5b8e271f16df3040d
Ivie Osoiye <ivieosoiye@gmail.com>
2022-08-10 22:01:13 +0100

    updated setting

--- django_project/settings.py
+++ django_project/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2i9p@trng=+8csu!xi^fy5cmy%w^s(vk_w4n2+i$!s@8)#c&*%'
+# SECRET_KEY = 'django-insecure-2i9p@trng=+8csu!xi^fy5cmy%w^s(vk_w4n2+i$!s@8)#c&*%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['thepetalblog.herokuapp.com']
 
 
 # Application definition
