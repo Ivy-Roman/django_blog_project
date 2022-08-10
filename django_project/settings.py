7ee3c58fef898e6ffdc1ddb844fe3e6de1e05c82
Ivie Osoiye <ivieosoiye@gmail.com>
2022-08-10 20:57:02 +0100

    added some AWS S3 settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'storages'
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -145,8 +145,24 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+# AWS_S3_OBJECT_PARAMETERS = {
+#     'CacheControl': 'max-age=86400'
+# }
+
+# AWS_LOCATION = 'static'
+
+# AWS_QUERYSTRING_AUTH = False
+
+# AWS_HEADERS = {
+#     'Access-Control-Allow-Origin': '*',
+# }
 
 AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
+AWS_DEFAULT_ACL = "public-read"
+AWS_S3_REGION_NAME = "eu-west-2"
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# STATIC_FILE_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
