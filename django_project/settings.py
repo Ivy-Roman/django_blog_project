1f8a1517ed34b2dcbf3841ee3725ae41fb7e079c
Ivie Osoiye <ivieosoiye@gmail.com>
2023-02-02 16:33:07 +0100

    added a dotenv module and added a .env file

--- django_project/settings.py
+++ django_project/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-2i9p@trng=+8csu!xi^fy5cmy%w^s(vk_w4n2+i$!s@8)#c&*%'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
